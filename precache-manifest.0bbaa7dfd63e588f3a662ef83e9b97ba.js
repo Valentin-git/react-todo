43cd65804bf159d1b7f370294df2903d641b1cdf
Valentin <Valentin@gmail.com>
2020-09-30 23:39:12 +0300

    Updates

---
+++ precache-manifest.0bbaa7dfd63e588f3a662ef83e9b97ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b195991100033a5eed3dd32e96a4f98",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "a29ef2f2aafc9bb36453",
+    "url": "/react-todo/static/css/main.1c36418e.chunk.css"
+  },
+  {
+    "revision": "f84ca3bc0fc82fe50c67",
+    "url": "/react-todo/static/js/2.e6a1fc27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.e6a1fc27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a29ef2f2aafc9bb36453",
+    "url": "/react-todo/static/js/main.393be545.chunk.js"
+  },
+  {
+    "revision": "b81e150b308975bc2389",
+    "url": "/react-todo/static/js/runtime-main.89ae8f88.js"
+  }
+]);
